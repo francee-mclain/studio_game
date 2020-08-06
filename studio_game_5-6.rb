98de2ca83af75bbb7b3ff44228c24b9c31ca3bd4
francee-mclain <65931740+francee-mclain@users.noreply.github.com>
2020-08-05 22:04:39 -0500

    Add files via upload

---
+++ studio_game_5-6.rb
@@ -0,0 +1,15 @@
+def say_hello(name, health=100)
+    "Hello, I'm #{name.capitalize} with a health of #{health} as of #{time}."
+end 
+
+
+def time
+    current_time = Time.new
+    current_time.strftime("%I:%M:%S")
+end 
+
+
+puts say_hello("larry", 60)
+puts say_hello("curly", 125)
+puts say_hello("moe")
+puts say_hello("shemp", 90)
