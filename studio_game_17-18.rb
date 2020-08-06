29a4ccf3726f4ab8dbf11fe098c2e4b6bc98386e
francee-mclain <65931740+francee-mclain@users.noreply.github.com>
2020-08-06 16:09:47 -0500

    Add files via upload

---
+++ studio_game_17-18.rb
@@ -0,0 +1,14 @@
+require_relative 'player' 
+require_relative 'game'
+require_relative 'treasuretrove'
+
+player1 = Player.new("moe")
+player2 = Player.new("curly", 125)
+player3 = Player.new("larry", 60)
+
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play(2)
+knuckleheads.print_stats
